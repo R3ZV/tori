d320fb02c47062881884a1e3e4bf3b8f5b30d9a6
R3ZV <razvan.maracine@protonmail.com>
2025-08-22 21:31:08 +0300

    feat: list decoding

--- src/decoder.zig
+++ src/decoder.zig
@@ -2,18 +2,50 @@ const std = @import("std");
 const Allocator = std.mem.Allocator;
 const ArrayList = std.ArrayList;
 const assert = std.debug.assert;
+const Writer = std.Io.Writer;
+
+pub const Error = error {
+    empty_root,
+    invalid_root,
+    invalid_number,
+    invalid_encoding,
+};
 
 const Self = @This();
 
 const ElementType = enum {
     str,
     number,
+    list,
 };
 
-const Element = union(ElementType) {
+pub const Element = union(ElementType) {
     str: []const u8,
     number: i64,
+    list: ArrayList(Element),
 };
+
+pub fn print(self: *const Element, stdout: *Writer) !void {
+    try _print(self, stdout);
+    try stdout.print("\n", .{});
+}
+
+fn _print(self: *const Element, stdout: *Writer) !void {
+    switch (self.*) {
+        .str => |val| try stdout.print("{s}", .{val}),
+        .number => |val| try stdout.print("{}", .{val}),
+        .list => |vals| {
+            try stdout.print("[", .{});
+            for (vals.items, 0..) |val, i| {
+                try _print(&val, stdout);
+                if (i + 1 != vals.items.len) {
+                    try stdout.print(", ", .{});
+                }
+            }
+            try stdout.print("]", .{});
+        },
+    }
+}
 
 pos: usize = 0,
 blob: []const u8,
@@ -27,16 +59,44 @@ pub fn init(blob: []const u8, alloc: Allocator) Self {
 }
 
 pub fn run(self: *Self) !Element {
+    if (self.blob.len == 0) {
+        return Error.empty_root;
+    }
+
     const ch = self.blob[self.pos];
+    if (ch == 'e') {
+        return Error.invalid_encoding;
+    }
+
     if (ch == 'i') {
         return self.decode_number();
     }
-    // } else if (std.ascii.isDigit(ch)) {
-    // }
-    return try self.decode_string();
+
+    if (ch == 'l') {
+        self.pos += 1;
+        var elems: ArrayList(Element) = .empty;
+        while (self.pos < self.blob.len and self.blob[self.pos] != 'e') {
+            const elem = try self.run(); 
+            std.debug.print("List item: {}\n", .{elem});
+            try elems.append(self.alloc, elem);
+            self.pos += 1;
+        }
+        self.pos += 1;
+        return Element {.list = elems };
+    }
+
+    if (ch == 'd') {
+        unreachable;
+    }
+
+    if (std.ascii.isDigit(ch)) {
+        return try self.decode_string();
+    }
+
+    return Error.invalid_root;
 }
 
-fn decode_number(self: *Self) Element {
+fn decode_number(self: *Self) Error!Element {
     // skip the 'i'
     self.pos += 1;
 
@@ -50,7 +110,9 @@ fn decode_number(self: *Self) Element {
     var num: i64 = 0;
     while (self.blob[self.pos] != 'e') : (self.pos += 1) {
         const ch = self.blob[self.pos];
-        assert(std.ascii.isDigit(ch));
+        if (!std.ascii.isDigit(ch)) {
+            return Error.invalid_number;
+        }
 
         // 48 is ascii for 0
         num = num * 10 + (self.blob[self.pos] - 48);
@@ -104,6 +166,7 @@ test "parsing numbers" {
         switch (expected[i]) {
             .number => assert(expected[i].number == value.number),
             .str => unreachable,
+            .list => unreachable,
         }
     }
 }
